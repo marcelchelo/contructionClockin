364ba389a01b86350b37c360fe8aafd5a2fcb080
wafranz <franzwaldir@hotmail.com>
2020-05-27 16:26:38 -0400

    cambio de nombre de la clase

--- Hello.java
+++ Hello.java
@@ -1,13 +1,8 @@
 import java.util.Scanner;
 
-public class lospendejos{
-<<<<<<< HEAD
-
-
-=======
+public class Hello{
 public static void main (){
     System.out.println("Putaso")
 }
->>>>>>> e79ae299140eb14a0f8e66e57f8224032a8ae5ea
     
 }
