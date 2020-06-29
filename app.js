8cc2d634c15fc5639909baacf5fa2e30a11d85a2
Marcelo <marcelo95326@icloud.com>
2020-06-29 16:41:02 -0400

    migration to userjs
    removed db
    post route to create user
    global variables commented out, not sure what that is for.

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
-const flash = require('connect-flash');
 //const session = require('express-session');
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 //const passport = require('passport');
 const mysql  = require('mysql')
 
@@ -18,14 +17,6 @@ const router = require("./routes/user")
 
 //Passport Config
 //  require('./config/passport')(passport)
-
-const db = mysql.createConnection({
-    host     : process.env.DB_HOST,
-    user     : process.env.DB_USER,
-    password : process.env.DB_PASS,
-    database : process.env.DB_SCHEMA
-});
-
 
 
 //Static folder
@@ -45,108 +36,19 @@ app.use(router)
 
 app.set('view engine', 'handlebars');
 
-// //Express session middleware
-// app.use(session({
-//     secret: 'secret',
-//     resave: true,
-//     saveUninitialized: true
-// }));
-
-// //Passport middleware
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-app.use(flash());
-
-//Global variables
-app.use(function(req, res, next){
-        res.locals.success_msg = req.flash('success_msg');
-        res.locals.error_msg = req.flash('error_msg');
-        res.locals.error =req.flash('error');
-        res.locals.user = req.user || null;
-        next();
-});
+// //Global variables
+// app.use(function(req, res, next){
+//         res.locals.success_msg = req.flash('success_msg');
+//         res.locals.error_msg = req.flash('error_msg');
+//         res.locals.error =req.flash('error');
+//         res.locals.user = req.user || null;
+//         next();
+// });
 
 
 
-//New Account POST request
-app.post('/user/newAccount',(req,res) => {
-    let errors = [];
 
-    if(req.body.password != req.body.password2){
-        errors.push({text: 'Password does not match'})
-    }
-    if(req.body.password.length < 4){
-        errors.push({text: 'Password must be at least 4 characters'})
-    }
-
-    if(errors.length > 0){
-        res.render('users/createUser', {
-            errors: errors,
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            password2: req.body.password2
-        });
-    }else{
-        //When email exists we do not get notified it does
-        //when email exists and we try to sign up(1) No notification (2) We try new email, notification of email in use appears. Corresponding to first email. 
-
-        let sql =`SELECT * FROM  user WHERE email = '${req.body.email}'`;
-        let query = db.query(sql, (err,result)=>{
-            if(err) throw err;
-            if(result.length > 0){
-                req.flash('error_msg', 'Email already in use');
-                res.redirect('/user/createAccount');    
-            }else{
-                let accountData = { email : req.body.email,
-                    password : req.body.password,
-                    type : 'business' }
-                    bcrypt.genSalt(10, (err, salt)=>{
-                        bcrypt.hash(accountData.password, salt, (err, hash)=>{
-                            if(err) throw err;
-                            accountData.password = hash;
-                            let sql = 'INSERT INTO user SET ?';
-                            let query = db.query(sql, accountData, (err,results) => {
-                            if (err) throw err;
-                                });
-                            req.flash('success_msg', 'User created');
-                            res.render('business/confirmation')
-                           // res.redirect('/user/createAccount')
-                        });
-        
-                    });
-            }
-        });
-
-        }
-
-         
-    });
-
-    // //Login form POST  
-    // app.post('/user/login', (req, res, next) => {
-    //     passport.authenticate('local', {
-    //         successRedirect: '/loginPage',
-    //         failureRedirect: '/users/login',
-    //         failureFlash: true
-    //     })(req, res, next);
-    // })
-   
-
-// // Login User Page
-// app.get('/loginPage', ensureAuthenticated, (req, res)=>{
-//     res.render('users/loginPage');
-// })
-
-//Logout User
-app.get('/logout', (req, res)=>{
-    req.logOut();
-    req.flash('success_msg', 'You are logged out');
-    res.redirect('/users/login')
-})
-   
-
+  
 
 const port =process.env.PORT || 3000;
 
--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,16 @@
 //will contain all of user related routes.  EX.  Login, Sign UP, Sign Out
 //Add New Employees
 
-
 const express =require('express')
 const router = express.Router()
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 const mysql = require('mysql')
 const randomize = require('randomatic')
 const passport = require('passport');
 const session = require('express-session');
-
+const bcrypt = require('bcryptjs');
+const flash = require('connect-flash');
 
 
 require('dotenv').config()
@@ -20,7 +21,7 @@ const {ensureAuthenticated} = require('../helpers/auth')
 
 const pool = mysql.createPool({
     
-    connectionLimit : 50, //number of connections that can run on parallel. 
+    connectionLimit : 50,  //number of connections that can run on parallel. 
     host            : process.env.DB_HOST,
     user            : process.env.DB_USER,
     password        : process.env.DB_PASS,
@@ -28,12 +29,12 @@ const pool = mysql.createPool({
 })
 
 
-
 //If we move database credentials to another file databasa.js   and store methods there to run queries this would be the code.
 //const myDatabase = new require('./database')      //   NEW, makes it an object  If we remove it wont work. 
 
 //connect DB test  using above code
 //myDatabase.Connect;
+
 
 //Passport Config
 require('../config/passport')(passport)   //..  To got up a level in directory
@@ -53,8 +54,7 @@ router.use(passport.session());
 
 
 
-
-
+router.use(flash());
 
 //Index route
 router.get('/',(req, res)=> {
@@ -77,15 +77,76 @@ router.get("/users", (req,res) =>{
 })
 
 
-
-
 //Create Account form
 router.get('/user/createAccount', (req,res)=>{
     res.render('users/createUser');
 })
 
+//i dont think this is needed,  what is it for ?
+router.use(function(req, res, next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error =req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
 
+//New Account POST request
+router.post('/user/newAccount',(req,res) => {
+    let errors = [];
 
+    if(req.body.password != req.body.password2){
+        errors.push({text: 'Password does not match'})
+    }
+    if(req.body.password.length < 4){
+        errors.push({text: 'Password must be at least 4 characters'})
+    }
+
+    if(errors.length > 0){
+        res.render('users/createUser', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2
+        });
+    }else{
+        //When email exists we do not get notified it does
+        //when email exists and we try to sign up(1) No notification (2) We try new email, notification of email in use appears. Corresponding to first email. 
+
+        let sql =`SELECT * FROM  user WHERE email = '${req.body.email}'`;
+        let query = pool.query(sql, (err,result)=>{
+            if(err) throw err;
+            if(result.length > 0){
+                req.flash('error_msg', 'Email already in use');
+                res.redirect('/user/createAccount');    
+            }else{
+                let accountData = { email : req.body.email,
+                    password : req.body.password,
+                    type : 'business' }
+                    bcrypt.genSalt(10, (err, salt)=>{
+                        bcrypt.hash(accountData.password, salt, (err, hash)=>{
+                            if(err) throw err;
+                            accountData.password = hash;
+                            let sql = 'INSERT INTO user SET ?';
+                            let query = pool.query(sql, accountData, (err,results) => {
+                            if (err) throw err;
+                                });
+                            req.flash('success_msg', 'User created');
+                            res.render('business/confirmation')
+                           // res.redirect('/user/createAccount')
+                        });
+        
+                    });
+            }
+        });
+
+        }
+
+         
+});
+
+  
 
 //Add Business Form
 router.get('/user/newBusiness', (req,res)=>{
@@ -116,7 +177,6 @@ router.post('/user/AddBusiness', (req, res) => {
 });
 
 
-
 // User login FORM
 router.get('/users/login', (req, res)=>{
     console.log('At the log in page')
@@ -139,4 +199,13 @@ router.get('/loginPage', ensureAuthenticated, (req, res)=>{
     res.render('users/loginPage');
 })
 
+
+
+//Logout User
+router.get('/logout', (req, res)=>{
+    req.logOut();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login')
+})
+   
 module.exports =router
